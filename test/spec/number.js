29f78df0797329be31dd412c370dbc711a631328
tristen <tristen.brown@gmail.com>
2018-09-14 15:35:30 -0400

    attempt tests against currency regex

--- test/spec/number.js
+++ test/spec/number.js
@@ -29,29 +29,23 @@ tape('sorts numbers', function(t) {
 
 tape('sorts currency', function(t) {
   // Number column
-  var el = table.querySelector('th:nth-child(4)');
+  var el = tableCurrency.querySelector('th');
   var event = document.createEvent('HTMLEvents');
   event.initEvent('click', true, false);
   el.dispatchEvent(event);
 
-  // Test that basic numbers sort
-  t.equal(table.rows[1].cells[4].innerHTML, '$0.79');
-  t.equal(table.rows[2].cells[4].innerHTML, '£2.79');
-  t.equal(table.rows[3].cells[4].innerHTML, '63€');
+  t.equal(tableCurrency.rows[1].cells[0].innerHTML, '63€');
+  t.equal(tableCurrency.rows[2].cells[0].innerHTML, '₽3.12');
+  t.equal(tableCurrency.rows[3].cells[0].innerHTML, '£2.79');
+  t.equal(tableCurrency.rows[4].cells[0].innerHTML, '$0.79');
 
   event.initEvent('click', true, false);
   el.dispatchEvent(event);
 
-  t.equal(table.rows[1].cells[4].innerHTML, '63€');
-  t.equal(table.rows[2].cells[4].innerHTML, '£2.79');
-  t.equal(table.rows[3].cells[4].innerHTML, '$0.79');
-
-  event.initEvent('click', true, false);
-  el.dispatchEvent(event);
-
-  t.equal(table.rows[1].cells[4].innerHTML, '$0.79');
-  t.equal(table.rows[2].cells[4].innerHTML, '£2.79');
-  t.equal(table.rows[3].cells[4].innerHTML, '63€');
+  t.equal(tableCurrency.rows[1].cells[0].innerHTML, '$0.79');
+  t.equal(tableCurrency.rows[2].cells[0].innerHTML, '£2.79');
+  t.equal(tableCurrency.rows[3].cells[0].innerHTML, '₽3.12');
+  t.equal(tableCurrency.rows[4].cells[0].innerHTML, '63€');
 
   t.end();
 });
